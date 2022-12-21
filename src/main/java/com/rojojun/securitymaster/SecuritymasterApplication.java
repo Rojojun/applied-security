b7edf87628d859bf6525b327704cdbdbf90176e8
rojojun <zoloman316@gmail.com>
2022-12-21 22:33:43 +0900

    feat> deprected 된 code -> filterchain방식 변경

---
+++ src/main/java/com/rojojun/securitymaster/SecuritymasterApplication.java
@@ -0,0 +1,13 @@
+package com.rojojun.securitymaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritymasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritymasterApplication.class, args);
+	}
+
+}
