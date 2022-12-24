3f5cda66e24e56a0544f7082c1325bae82ca4329
rojojun <zoloman316@gmail.com>
2022-12-25 00:43:58 +0900

    Security FormLogin 관련 설정 사항 + 익명클래스

--- src/main/java/com/rojojun/securitymaster/SecurityConfig.java
+++ src/main/java/com/rojojun/securitymaster/SecurityConfig.java
@@ -1,10 +1,20 @@
 package com.rojojun.securitymaster;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationEntryPointFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -16,7 +26,30 @@ public class SecurityConfig {
                 .authorizeHttpRequests()
                 .anyRequest().authenticated();
         http
-                .formLogin();
+                .formLogin()
+                //.loginPage("/loginPage")    //누구나 접근이 가능하도록 해야함
+                .defaultSuccessUrl("/") //인증이 성공시 Default로 가는 URL
+                .failureUrl("/login")
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc")
+                .successHandler(new AuthenticationSuccessHandler() {
+                    // 익명클래스를 사용하여 전달하는 방식
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        // Log로 대체 예정 ->
+                        System.out.println("authentication" + authentication.getName());    //인증에 성공한 유저의 이름을 반환
+                        response.sendRedirect("/");
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                        System.out.println("exception" + exception.getMessage());   //인증 실패에 대한 예외 메시지 반환
+                        response.sendRedirect("/loginPage");
+                    }
+                })
+                .permitAll();
         return http.build();
     }
 }
