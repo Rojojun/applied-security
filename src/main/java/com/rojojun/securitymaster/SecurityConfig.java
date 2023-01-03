6f5684da003e6b679d4c659e51706ae26b727cf0
rojojun <zoloman316@gmail.com>
2023-01-03 23:43:21 +0900

    feat Remeber-Me 인증

--- src/main/java/com/rojojun/securitymaster/SecurityConfig.java
+++ src/main/java/com/rojojun/securitymaster/SecurityConfig.java
@@ -4,12 +4,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationEntryPointFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -22,6 +25,7 @@ import java.io.IOException;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -72,6 +76,11 @@ public class SecurityConfig {
                     }
                 })
                 .deleteCookies("remember-me");
+        http
+                .rememberMe()
+                .rememberMeParameter("remeber")
+                .tokenValiditySeconds(3600)
+                .userDetailsService(userDetailsService);
         return http.build();
     }
 }
